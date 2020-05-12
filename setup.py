e33839a94d65343e258d650b673ec6e293f9874a
Gurvan <gurvanpriem@gmail.com>
2020-05-12 16:10:27 +0200

    gitignore

--- setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='mca', version='0.1', packages=['mca'])
--- start_training.sh
+++
@@ -1,1 +0,0 @@
-python -m mca.controller.train --load-controller "/home/gurvan/Dev/model-controller-action/mca/controller/results/good_falcon_bis2/models/controller.pth" 
