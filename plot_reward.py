692821a23b11f7cc5504ccff65eb8321403bbb61
Gurvan <gurvanpriem@gmail.com>
2020-05-13 15:47:25 +0200

    Add total reward to plot_reward.py

--- plot_reward.py
+++ plot_reward.py
@@ -28,15 +28,20 @@ if __name__ == "__main__":
         plt.show()
 
     if reward.shape[1] > 2:
-        plt.subplot(211)
+        plt.subplot(311)
         plt.plot(reward[args.begin:, 0], reward[args.begin:, 1], alpha=0.3)
         plt.plot(reward[args.begin:, 0], ema(reward[args.begin:, 1], args.smooth), alpha=0.3)
         plt.ylabel("Optimistic reward")
 
-        plt.subplot(212)
+        plt.subplot(312)
         plt.plot(reward[args.begin:, 0], reward[args.begin:, 2], alpha=0.3)
         plt.plot(reward[args.begin:, 0], ema(reward[args.begin:, 2], args.smooth), alpha=0.3)
-        plt.xlabel("Total frames")
         plt.ylabel("Bonus reward")
+
+        plt.subplot(313)
+        plt.plot(reward[args.begin:, 0], reward[args.begin:, 1] + reward[args.begin:, 2], alpha=0.3)
+        plt.plot(reward[args.begin:, 0], ema(reward[args.begin:, 1] + reward[args.begin:, 2], args.smooth), alpha=0.3)
+        plt.xlabel("Total frames")
+        plt.ylabel("Total reward")
 
         plt.show()
