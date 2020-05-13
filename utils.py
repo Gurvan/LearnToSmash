9fc58302ebf0cec56e1913f8cad85861829a3233
Gurvan <gurvanpriem@gmail.com>
2020-05-13 13:41:52 +0200

    properly scale distance in bonus

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def proximity_bonus(observations, act_every, alpha=0.1, agent_x_index=1, agent_y
     index = torch.tensor([agent_x_index, agent_y_index, opponent_x_index, opponent_y_index]).long().to(observations.device)
     agent_x, agent_y, opponent_x, opponent_y = observations.index_select(dim=-1, index=index).split(1, dim=-1)
     d = ((agent_x - opponent_x).pow(2) + (agent_y - opponent_y).pow(2)).sqrt().squeeze(-1)
-    bonus = 1/60 * alpha * 1/(1 + alpha * d)
+    bonus = 1/60 * alpha * 1/(1 + alpha * d / 100.)
 
     n = int(observations.shape[0] / act_every)
     bonus = bonus[1:].view(n, act_every, -1).sum(1)
